49079bfcc86fbc8b563b9ac7ca295252238fb8b5
Neile0 <aidan.neil101@gmail.com>
2019-04-15 11:57:36 +0100

    Add files via upload

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,15 @@
 $( document ).ready(function() {
+	console.log("JS Ready");
+
 	// navigation scroll
 	$('.scroll').click(function(event) {
 	var id = $(this).attr("href");
-	//var offset = 70;
-	var target = $(id).offset().top //- offset;
+	var offset = 100;
+	var target = $(id).offset().top - offset;
 	$('html, body').animate({
 		scrollTop: target
 	}, 500);
 	event.preventDefault();
+	});
 
 });
